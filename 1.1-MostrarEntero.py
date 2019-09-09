8888b4a7adb617be5e4e53a9d24cf6121a50d1ef
marcelofabiangutierrez88 <marcelofabiangutierrez88@gmail.com>
2019-09-09 16:41:19 -0300

    Create 1.1-MostrarEntero.py

---
+++ 1.1-MostrarEntero.py
@@ -0,0 +1,15 @@
+'''
+1.1 Desarrollar un programa que solicite por pantalla(consola)el ingreso de un numero entero. Luego el programa debe mostrar por pantalla
+(consola)el numero ingresado.
+'''
+#def mostrarEntero(numero):
+#    numero=int (input("Ingrese número: "))
+#    print("Usted ingresó: ",numero)
+#def main():
+#    mostrarEntero(252)
+#main()
+
+def main():
+    numero = int(input("Ingrese Número: "))
+    print("Usted ingresó ",numero)
+main()
