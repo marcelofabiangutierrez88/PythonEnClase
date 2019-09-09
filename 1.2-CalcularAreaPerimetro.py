36a8104181df29b5c1926c0cf9b3b0350c53e886
marcelofabiangutierrez88 <marcelofabiangutierrez88@gmail.com>
2019-09-09 16:41:40 -0300

    Create 1.2-CalcularAreaPerimetro.py

---
+++ 1.2-CalcularAreaPerimetro.py
@@ -0,0 +1,26 @@
+#def pedirDatos():
+#    lado1=int(input("Ingresar lado1: "))
+#    lado2=int(input("Ingresar lado2: "))
+#    
+#def calcularArea(lado1,lado2):
+#    area = lado1*lado2
+#    print("El Area: ",area)
+#    return area
+#def calcularPerimetro(lado1,lado2):
+#    perimetro=(lado1*2)+(lado2*2)
+#    print("El Perimetro: ",perimetro)
+#    return perimetro
+#def main():
+#    pedirDatos()
+#    calcularArea(50,10)
+#    calcularPerimetro(50,10)
+#main()
+
+def main():
+    lado1=int(input("Ingresar Lado1: "))
+    lado2=int(input("Ingesar Lado2: "))
+    area=lado1*lado2
+    perimetro=(lado1*2)+(lado2*2)
+    print("El Area: ",area)
+    print("El perimetro: ",perimetro)
+main()
